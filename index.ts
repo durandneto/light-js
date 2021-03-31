3c0dfc59bf561e7479a22c4d2b080b18234d9d2c
durandneto <durand.neto@gmail.com>
2021-03-31 13:34:08 -0400

    1 file added, 1 deleted

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import DOMRender from "./Render";
 import User from "./User";
 
-DOMRender.render(<User />, document.getElementById("app"));
+DOMRender.render(User, document.getElementById("app"));
+
+
